a12ceb9c6d4f31157208a71f847ef92c74137517
AssiaIlieva <ac17@abv.bg>
2025-01-31 13:47:44 +0200

    First exercise of topic 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+const heightMark = 1.69;
+const weightMark = 78;
+const heigthJohn = 1.95;
+const weightJohn = 95;
+
+let BMI_Mark = weightMark / heightMark ** 2;
+console.log(BMI_Mark);
+
+let BMI_John = weightJohn / heigthJohn ** 2;
+
+console.log(BMI_John);
