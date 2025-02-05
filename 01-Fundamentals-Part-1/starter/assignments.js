2d6cb98d5cd2c99a74335112b5f51aa8b0c91c83
AssiaIlieva <ac17@abv.bg>
2025-02-05 17:25:47 +0200

    Update assignments

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -2,14 +2,18 @@ const country = 'Bulgaria';
 const continent = 'Europe';
 const population = 6;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 const isIsland = false;
 let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+const halfPopulation = population / 2;
+population++;
+console.log(population > 6);
+console.log(population < 33);
