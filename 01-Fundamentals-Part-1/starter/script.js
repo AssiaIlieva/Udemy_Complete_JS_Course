0e580391d2d951a22bdd8f43d00251a087913608
AssiaIlieva <ac17@abv.bg>
2025-02-20 15:00:52 +0200

    If / else statement exercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,39 @@
-const heightMark = 1.69;
-const weightMark = 78;
-const heigthJohn = 1.95;
-const weightJohn = 95;
+// const heightMark = 1.69;
+// const weightMark = 78;
+// const heigthJohn = 1.95;
+// const weightJohn = 95;
 
-let BMI_Mark = weightMark / heightMark ** 2;
-console.log(BMI_Mark);
+// let BMI_Mark = weightMark / heightMark ** 2;
+// console.log(BMI_Mark);
 
-let BMI_John = weightJohn / heigthJohn ** 2;
+// let BMI_John = weightJohn / heigthJohn ** 2;
 
-console.log(BMI_John);
+// console.log(BMI_John);
+// console.log('This is \nmulti-line \nstring');
+// console.log(
+//   `This is
+// a multi-line
+// string`
+// );
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log('Sarah can start driving license 🚗');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 1991;
+
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
