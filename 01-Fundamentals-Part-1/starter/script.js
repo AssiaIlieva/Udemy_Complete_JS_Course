dbf359c3a3f27d65a63d05fcd2c48d22b7c2bacc
AssiaIlieva <ac17@abv.bg>
2025-02-21 15:01:56 +0200

    Equality operators exercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -39,45 +39,79 @@
 // console.log(century);
 
 //type conversion
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
-console.log(Number('Jonas'));
-console.log(typeof NaN);
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+//console.log(String(23), 23);
 
 //type coercion
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' + '10' + 3);
-console.log('23' * '2');
-console.log('23' / '2');
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' + '10' + 3);
+// console.log('23' * '2');
+// console.log('23' / '2');
 
-let n = '1' + 1;
-n = n - 1;
-console.log(n);
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n);
 
-console.log(2 + 3 + 4 + '5');
-console.log('10' - '4' - '3' - 2 + '5');
+// console.log(2 + 3 + 4 + '5');
+// console.log('10' - '4' - '3' - 2 + '5');
 
 // 5 falsy values: 0, ' ', undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
-console.log(Boolean({}));
-console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
 
-const money = 0;
-if (money) {
-  console.log("Don't spend it all");
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all");
+// } else {
+//   console.log('You should get a job!');
+// }
+
+// let height;
+// if (height) {
+//   console.log('YAY! Height is defined');
+// } else {
+//   console.log('Height is UNDEFINED');
+// }
+
+//Equality Operators == vs. ===
+
+// const age = '18';
+// if (age === 18) console.log('You just became an adult (strict)');
+// if (age == 18) console.log('You just became an adult (loose)');
+
+// const favourite = prompt("What's your favourite number?");
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite == 23) {
+//   // '23' == 23
+//   console.log('Cool! 23 is an amazing number!');
+// }
+// if (favourite === 23) {
+//   // '23' ≠ 23
+//   console.log('Cool! 23 is an amazing number!');
+// }
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  // it will be a number and 23 === 23
+  console.log('Cool! 23 is an amazing number!');
+} else if (favourite === 7) {
+  console.log('7 is alse a cool number');
 } else {
-  console.log('You should get a job!');
+  console.log('Number is not 23 or 7');
 }
 
-let height;
-if (height) {
-  console.log('YAY! Height is defined');
-} else {
-  console.log('Height is UNDEFINED');
-}
+if (favourite !== 23) console.log('Why not 23?');
