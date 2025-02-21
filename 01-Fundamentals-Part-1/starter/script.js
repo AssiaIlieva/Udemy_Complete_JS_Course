717078be278c7871d199e4e94a429ddb2927b25b
AssiaIlieva <ac17@abv.bg>
2025-02-21 13:24:14 +0200

    Truthy and Falsy Values exercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -60,3 +60,24 @@ console.log(n);
 
 console.log(2 + 3 + 4 + '5');
 console.log('10' - '4' - '3' - 2 + '5');
+
+// 5 falsy values: 0, ' ', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
