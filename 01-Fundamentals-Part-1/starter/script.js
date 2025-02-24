cefc4757c52c222893a2e4a789709e761b7516b7
AssiaIlieva <ac17@abv.bg>
2025-02-24 12:05:30 +0200

    The Conditional (Ternary) Operator exercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -140,27 +140,39 @@
 //   console.log('Someone else should drive...');
 // }
 
-const day = 'friday';
+// const day = 'friday';
 
-switch (day) {
-  case 'monday': //day === 'monday'
-    console.log('Plan course structure');
-    console.log('Go to coding meetup');
-    break;
-  case 'tuesday':
-    console.log('Prepare theory videos');
-    break;
-  case 'wednesday':
-  case 'thurday':
-    console.log('Write code examples');
-    break;
-  case 'friday':
-    console.log('Record videos');
-    break;
-  case 'saturday':
-  case 'sunday':
-    console.log('Enjoy the weekend');
-    break;
-  default:
-    console.log('Not a valid day!');
-}
+// switch (day) {
+//   case 'monday': //day === 'monday'
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wednesday':
+//   case 'thurday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend');
+//     break;
+//   default:
+//     console.log('Not a valid day!');
+// }
+
+// Conditional (Ternary) Operator
+
+const age = 15;
+age >= 18
+  ? console.log('I like to drink wine 🍷')
+  : console.log('I like to drink water 🥛');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 🥛';
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 🥛'}`);
