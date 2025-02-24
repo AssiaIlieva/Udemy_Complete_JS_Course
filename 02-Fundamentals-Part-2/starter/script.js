d519af1089d8bf03d04bd40ffd686d2f4c11a96b
AssiaIlieva <ac17@abv.bg>
2025-02-24 15:23:44 +0200

    Examples Function declaration & Function expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,22 +10,36 @@
 
 //FUNCTIONS
 
-function logger() {
-  console.log('My name is Jonas');
+// function logger() {
+//   console.log('My name is Jonas');
+// }
+
+// //calling / running / invoking function
+
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
+const age1 = calcAge1(1991);
 
-//calling / running / invoking function
+//Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
 
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+console.log(age1, age2);
