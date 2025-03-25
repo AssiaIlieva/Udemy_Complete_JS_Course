e96dc7d2a08469361695c455ae4eeedb2f9b484d
AssiaIlieva <ac17@abv.bg>
2025-03-25 13:19:04 +0200

    Solution of CHALLENGE #3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -261,3 +261,34 @@
 // console.log(jonas['calcAge']());
 // console.log(jonas.age);
 // console.log(jonas.getSummary());
+
+//CHALLENGE #3
+
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function () {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     }
+// };
+
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function () {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     }
+// };
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// console.log(
+//     mark.bmi > john.bmi
+//         ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+//         : `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+// );
