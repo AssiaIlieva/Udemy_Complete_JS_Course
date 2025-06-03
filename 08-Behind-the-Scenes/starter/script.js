271b6c62bb5542aa1baa0d2f738eb9a7e206d829
AssiaIlieva <ac17@abv.bg>
2025-06-03 15:30:02 +0300

    Scope exercise

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function calcAge(birthyear) {
+  const age = 2037 - birthyear;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthyear}`;
+    console.log(output);
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true;
+      //Creating New variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      //Reassigning oute scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    console.log(millenial);
+    console.log(output);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+
+calcAge(1991);
