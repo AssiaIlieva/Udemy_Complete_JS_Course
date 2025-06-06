770f444910a7d58668a13220bfb795f6a05a1161
AssiaIlieva <ac17@abv.bg>
2025-06-06 16:08:11 +0300

    This Keyword Exercise

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -26,7 +26,7 @@ function calcAge(birthyear) {
 
 const firstName = 'Jonas';
 
-calcAge(1991); */
+calcAge(1991); 
 
 ////////////////
 //Hoisting and TDZ in Practice
@@ -66,3 +66,42 @@ var numProducts = 10;
 function deleteShopCart() {
   console.log('All products deleted!');
 }
+  */
+
+////////////////////////////////////
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
