5b1ee025d9636ba83cd5131e68839a1550220f07
AssiaIlieva <ac17@abv.bg>
2025-06-09 15:36:52 +0300

    Regular and Arrow functions exercise

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -66,7 +66,7 @@ var numProducts = 10;
 function deleteShopCart() {
   console.log('All products deleted!');
 }
-  */
+
 
 ////////////////////////////////////
 // The this Keyword in Practice
@@ -105,3 +105,52 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+  */
+
+//////////////////////
+// Regular functions vs. Arrow Functions
+
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    //SOLUTION 1
+    // const self = this; // usually self or that is used
+    // const isMillenial = function () {
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    //SOLUTION2 - we use arrow funciton because it inherits this keyword from the paren scope
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+  greet: () => console.log(`Hey, ${this.firstName}`),
+};
+
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+
+const addExpr = function (a, b) {
+  console.log(arguments); //works only with function declaration and function expression, not arrow functions
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
